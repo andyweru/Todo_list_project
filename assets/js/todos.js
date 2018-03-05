803be8499f921b2be28e7c357fec090274cdb224
andyweru <andyweru@gmail.com>
2018-03-05 10:27:17 +0300

    todo list fully functional

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -19,3 +19,8 @@ $("input[type = 'text']").keypress(function(event){
     $("ul").append("<li><span><i class = 'fa fa-trash'></i></span>" + todoText + "</li>");
   }
 })
+
+// fade input in and out on + button clicking
+$(".fa-plus").click(function(){
+  $("input[type = 'text']").fadeToggle();
+});
