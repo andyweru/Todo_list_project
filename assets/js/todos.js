e85371f660c04a020c64163985d50db439ac45a3
andyweru <andyweru@gmail.com>
2018-03-05 09:35:39 +0300

    todo list checkout and delete tasks functionality added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,13 @@
-alert("connected");
+// Check off specific todos by clicking
+
+$("li").click(function(){
+  $(this).toggleClass("completed");
+});
+
+// click on x to delete todo
+$("span").click(function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
